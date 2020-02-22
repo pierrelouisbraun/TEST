e73303ffc1d3ef33ee154f8860afe89d4941e484
pierrelouisbraun <38157266+pierrelouisbraun@users.noreply.github.com>
2020-02-22 13:32:24 +0100

    Update es_lang.php

--- es_lang.php
+++ es_lang.php
@@ -1,4 +1,4 @@
 <?php
-$xmlData = file_get_contents('http://challenge01.root-me.org/web-serveur/ch13/index.php');
-echo $xmlData;
+$Data = file_get_contents('http://challenge01.root-me.org/web-serveur/ch13/index.php');
+echo base64_encode($Data);
 ?>
